e48f08f35f04650c5e75a94ed47f33f6a9264edf
root <root@Jarvis.localdomain>
2019-01-14 04:03:52 +0200

    Change dir for logs

--- mylogger.py
+++ mylogger.py
@@ -4,12 +4,12 @@ import logging
 def mylogger(session, logger_level):
     if logger_level == 'debug':
         logger1 = logging.getLogger('debug')
-        logHandler = logging.FileHandler('logs/' + session + "_debug.log")
+        logHandler = logging.FileHandler(session + "_debug.log")
         formatter = logging.Formatter(
             '%(asctime)-15s:%(levelname)s:%(message)s')
     elif logger_level == 'info':
         logger1 = logging.getLogger('info')
-        logHandler = logging.FileHandler('logs/' + session + "_Armies.log")
+        logHandler = logging.FileHandler(session + "_Armies.log")
         formatter = logging.Formatter('%(levelname)s:%(message)s')
     logHandler.setFormatter(formatter)
     logger1.addHandler(logHandler)
